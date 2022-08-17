01312f310bf922070d4c275d5a03c878fada5950
KamchiDjunusov <dimooooon70@gmail.com>
2022-08-17 16:29:26 +0200

    github homeWork fragment

---
+++ app/src/main/java/com/example/todo/OnBoardingFragment.kt
@@ -0,0 +1,53 @@
+package com.example.todo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.todo.databinding.FragmentOnBoardingBinding
+
+class OnBoardingFragment : Fragment(), OnItemClicker {
+
+    private lateinit var binding: FragmentOnBoardingBinding
+    private val list = arrayListOf<OnBoardingModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentOnBoardingBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        list.add(OnBoardingModel(R.drawable.on_boarding_1, "Экономь время", "Дальше"))
+        list.add(OnBoardingModel(R.drawable.on_boarding_2, "Достигай целей", "Дальше"))
+        list.add(OnBoardingModel(R.drawable.on_boarding_3, "Развивайся", "Начинаем"))
+        binding.viewPager.adapter = OnBoardingAdapter(list, this)
+        binding.dotsIndicator.attachTo(binding.viewPager)
+    }
+
+    override fun onClick() {
+        findNavController().navigate(R.id.clearBackStack)
+
+    }
+
+    override fun onClickNext() {
+        onNext()
+    }
+
+    private fun onNext() {
+        val adapter = binding.viewPager.adapter
+        val currentPosition = binding.viewPager.currentItem
+        val nextPosition = currentPosition + 1
+        if (nextPosition < adapter?.itemCount!!) {
+            binding.viewPager.currentItem = nextPosition
+        } else
+            binding.viewPager.currentItem = 0
+    }
+}
+
